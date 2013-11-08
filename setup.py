cfbfca5cc7eb8895cce1daf0791025cfa432821b
Anton Novosyolov <anton.novosyolov@gmail.com>
2013-11-08 13:49:55 +0400

    Py3k compatibility

--- setup.py
+++ setup.py
@@ -16,13 +16,13 @@ setup(
     url='https://github.com/jmcclell/django-bootstrap-pagination',
     license='MIT licence, see LICENCE',
     description='Render Django Page objects as Bootstrap Pagination compatible HTML',
-    long_description=readme_text.encode('UTF-8'),
+    long_description=readme_text,
     zip_safe=False,
     include_package_data=True,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Framework :: Django",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",        
+        "License :: OSI Approved :: MIT License",
     ]
 )
